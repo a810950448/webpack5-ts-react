b607c1877896919d2af63e1d86caa8a050b238a7
what4up <a810950448@live.cn>
2020-09-13 11:33:52 +0800

    feat: 修正打包错误

--- build/webpack.config.prod.ts
+++ build/webpack.config.prod.ts
@@ -1,20 +1,24 @@
 import merge from "webpack-merge";
 import { config } from "./webpack.config";
-import CleanWebpackPlugin from "clean-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import path from "path";
 
 const prodConfig = merge(config, {
   mode: "production",
+  devtool: "eval",
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: path.join("../public/template.html"),
+      template: "public/template.html",
+      filename: "index.html",
+      chunks: "all",
     }),
-    // new MiniCssExtractPlugin({
-    //   filename: "css/[name].css",
-    //   chunkFilename: "css/[id].css",
-    // }),
+
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "css/[id].css",
+    }),
   ],
 });
 
--- build/webpack.config.ts
+++ build/webpack.config.ts
@@ -1,6 +1,5 @@
 import webpack from "webpack";
 import path from "path";
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config: webpack.Configuration = {
   entry: path.join("../src/index.tsx"), // 入口文件
@@ -13,17 +12,7 @@ const config: webpack.Configuration = {
       },
       {
         test: /\.(le|c)ss$/i,
-        use: [
-          "style-loader",
-          // 提取到单独的CSS文件
-          // MiniCssExtractPlugin.loader,
-          // 转换 CSS 到 CommonJS
-          "css-loader",
-          // 给 CSS 添加前缀以适应各浏览器
-          "postcss-loader",
-          // 编译 Less 到 CSS
-          "less-loader",
-        ],
+        use: ["style-loader", "css-loader", "postcss-loader", "less-loader"],
       },
       {
         test: /\.(png|jpe?g|gif|webp)$/i,
@@ -41,23 +30,12 @@ const config: webpack.Configuration = {
     ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"], // 按顺序解析以上扩展名的文件(必须添加，否则通过import进来的文件无法解析。import时可不写扩展名 默认值为 ['.wasm', '.mjs', '.js', '.json']
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "..", "dist"),
   },
-  // plugins: [
-  //   // new CleanWebpackPlugin(),
-  //   new HtmlWebpackPlugin({
-  //     template: "public/index.html",
-  //     title: "TypeScript-Webpack-starter",
-  //   }),
-  //   new MiniCssExtractPlugin({
-  //     filename: "css/[name].css",
-  //     chunkFilename: "css/[id].css",
-  //   }),
-  // ],
 };
 
 export { config };
